e01977072d12f0cf45a235cd4e662fdb45ed4479
G'k <epersonnal@keemail.me>
2021-03-09 19:15:22 +0100

    Add approval tests

--- tests/TestCase.cpp
+++ tests/TestCase.cpp
@@ -1,10 +1,23 @@
-#define CATCH_CONFIG_MAIN
+#define APPROVALS_CATCH // This tells Approval Tests to provide a main() - only do this in one cpp file
 
+#include "ApprovalTests.v.10.8.0.hpp"
 #include <catch2/catch.hpp>
 
 #include <NifFile.hpp>
 
 using namespace nifly;
+namespace fs = std::filesystem;
+using namespace ApprovalTests;
+
+template<>
+std::string ApprovalTests::StringMaker::toString(const NifFile& contents) {
+	std::stringstream stream;
+	auto copy = contents;
+	copy.Save(stream);
+	return stream.str();
+}
+
+const auto directoryDisposer = Approvals::useApprovalsSubdirectory("approval_tests");
 
 // WIP: Implement some proper testing
 
@@ -12,4 +25,32 @@ TEST_CASE("NifFile.Load (not existing)", "[NifFile]") {
 	NifFile test;
 	test.Load("not_existing.nif");
 	REQUIRE(!test.IsValid());
+}
+
+TEST_CASE("Optimize lots of meshes") {
+	OptOptions opts;
+	opts.removeParallax = false;
+	opts.targetVersion = NiVersion::getSSE();
+	ExceptionCollector exceptions; // Allow all files to be written, regardless of errors
+	const auto process = [&opts](const auto& dir) {
+		for (auto& p : fs::recursive_directory_iterator(dir)) {
+			SECTION(p.path().u8string()) {
+				if (!p.is_regular_file())
+					continue;
+
+				if (p.path().extension() != ".nif")
+					continue;
+
+				NifFile file(p.path());
+				SECTION("load") { Approvals::verify(file, Options(AutoApproveIfMissingReporter())); }
+				file.OptimizeFor(opts);
+				SECTION("optimize") { Approvals::verify(file, Options(AutoApproveIfMissingReporter())); }
+				file.Save("temp.nif");
+			}
+		}
+	};
+
+	process("INPUT/standard");
+	opts.headParts = true;
+	process("INPUT/headparts");
 }
