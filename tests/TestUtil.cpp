28b05560ee670a199a452252475c4be6b94f35da
ousnius <ousnius@users.noreply.github.com>
2024-06-09 16:03:35 +0200

    Log file sizes in test

--- tests/TestUtil.cpp
+++ tests/TestUtil.cpp
@@ -1,5 +1,6 @@
 #include "TestUtil.hpp"
 #include <fstream>
+#include <catch2/catch.hpp>
 
 bool CompareBinaryFiles(const std::filesystem::path& fileName1, const std::filesystem::path& fileName2) {
 	// Open file streams at the end of each file
@@ -7,7 +8,13 @@ bool CompareBinaryFiles(const std::filesystem::path& fileName1, const std::files
 	std::ifstream fileStream2(fileName2, std::ifstream::ate | std::ifstream::binary);
 
 	// Compare file size to start with
-	if (fileStream1.tellg() != fileStream2.tellg())
+	std::streampos fileSize1 = fileStream1.tellg();
+	std::streampos fileSize2 = fileStream1.tellg();
+
+	UNSCOPED_INFO("File size '" << fileName1.filename() << "': " << fileSize1);
+	UNSCOPED_INFO("File size '" << fileName2.filename() << "': " << fileSize2);
+
+	if (fileSize1 != fileSize2)
 		return false;
 
 	// Rewind to the beginning of each stream
